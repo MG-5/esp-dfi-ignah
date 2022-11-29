a8d1f4877fad89527577d4f506ee13757faf80d1
MG-5 <mg-95@t-online.de>
2022-11-29 21:29:58 +0100

    Add I²C accessors

---
+++ main/light_sensor/I2cBusAccessor.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+
+class I2cBusAccessor
+{
+public:
+    using DeviceAddress = uint16_t;
+
+    virtual void beginTransaction(DeviceAddress address) = 0;
+    virtual void endTransaction() = 0;
+
+    virtual bool read(uint8_t *buffer, uint16_t length) = 0;
+    virtual bool readFromRegister(uint8_t registerAdress, uint8_t *buffer, uint16_t length);
+    virtual bool readByteFromRegister(uint8_t registerAdress, uint8_t &byte) = 0;
+    virtual bool readWordFromRegister(uint8_t registerAdress, uint16_t &word) = 0;
+
+    virtual bool write(const uint8_t *data, uint16_t length) = 0;
+    virtual bool writeByteToRegister(uint8_t registerAdress, const uint8_t byte) = 0;
+    virtual bool writeWordToRegister(uint8_t registerAdress, const uint16_t word) = 0;
+};
