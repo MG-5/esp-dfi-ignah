7c6f8d3ac498852a34f1c2ee42509415b185eee0
MG-5 <mg-95@t-online.de>
2023-01-25 01:06:56 +0100

    Fix missing pure function and deconstruktor

--- main/light_sensor/I2cBusAccessor.hpp
+++ main/light_sensor/I2cBusAccessor.hpp
@@ -11,11 +11,13 @@ public:
     virtual void endTransaction() = 0;
 
     virtual bool read(uint8_t *buffer, uint16_t length) = 0;
-    virtual bool readFromRegister(uint8_t registerAdress, uint8_t *buffer, uint16_t length);
+    virtual bool readFromRegister(uint8_t registerAdress, uint8_t *buffer, uint16_t length) = 0;
     virtual bool readByteFromRegister(uint8_t registerAdress, uint8_t &byte) = 0;
     virtual bool readWordFromRegister(uint8_t registerAdress, uint16_t &word) = 0;
 
     virtual bool write(const uint8_t *data, uint16_t length) = 0;
     virtual bool writeByteToRegister(uint8_t registerAdress, const uint8_t byte) = 0;
     virtual bool writeWordToRegister(uint8_t registerAdress, const uint16_t word) = 0;
+
+    virtual ~I2cBusAccessor() = default;
 };
