645baa253d221b15c2379f060eee58b69f783716
tmwsnrx <xrnswmt@protonmail.com>
2023-01-27 19:52:54 +0100

    Set free text

--- web-ui/esp-dfi-ignah-ui/src/app/components/display/free-text/free-text.component.ts
+++ web-ui/esp-dfi-ignah-ui/src/app/components/display/free-text/free-text.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { getFreeText, selectFreeText, setFreeText } from 'src/app/store/mode';
 
 @Component({
   selector: 'app-free-text',
   templateUrl: './free-text.component.html',
   styleUrls: ['./free-text.component.scss']
 })
-export class FreeTextComponent {
+export class FreeTextComponent implements OnInit, OnDestroy {
 
   freeTextFormGroup = this.formBuilder.group({
     line1: '',
@@ -16,6 +19,43 @@ export class FreeTextComponent {
     line5: ''
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  freeTextSubscription$: Subscription = Subscription.EMPTY;
 
+  constructor(private formBuilder: FormBuilder, private store: Store) { }
+
+  ngOnInit(): void {
+    // this.store.dispatch(getFreeText());
+
+    this.store.select(selectFreeText).subscribe(freeText => {
+      const lines = {
+        line1: freeText.lines[0] ?? '',
+        line2: freeText.lines[1] ?? '',
+        line3: freeText.lines[2] ?? '',
+        line4: freeText.lines[3] ?? '',
+        line5: freeText.lines[4] ?? ''
+      }
+
+      this.freeTextFormGroup.patchValue(lines);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.freeTextSubscription$.unsubscribe();
+  }
+
+  saveFreeText(): void {
+    const lines = [
+      this.freeTextFormGroup.value.line1 ?? '',
+      this.freeTextFormGroup.value.line2 ?? '',
+      this.freeTextFormGroup.value.line3 ?? '',
+      this.freeTextFormGroup.value.line4 ?? '',
+      this.freeTextFormGroup.value.line5 ?? ''
+    ];
+
+    this.store.dispatch(setFreeText({
+      freeText: {
+        lines
+      }
+    }));
+  }
 }
